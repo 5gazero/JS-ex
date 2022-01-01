baa232d7c3c990e675dbd81b47daee85e8405ccb
5gazero <kayo9410@gmail.com>
2022-01-02 02:30:27 +0900

    [add]: Login, Clock, Quotes n Background

---
+++ js/greeting.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+// 똑같은 string을 반복해서 사용하는 경우에 변수로 지정(대문자가 관습)
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const onLoginSubmit = (event) => {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+};
+
+const paintGreetings = () => {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
+}
