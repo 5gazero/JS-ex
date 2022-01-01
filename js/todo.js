a6e79d7f106726c7fbeb63a3f1ce66bf5562a117
5gazero <kayo9410@gmail.com>
2022-01-02 03:15:03 +0900

    [feat]: Deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  // click에 대한 event
+  const li = event.target.parentElement; // 클릭 된 element의 부모
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "🗑";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
